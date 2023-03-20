a372517cd46e7a24e244faa4e29ceee60b1f2e0d
GabrielModog <criativo.modog@gmail.com>
2023-03-20 00:31:50 -0300

    feat: add save editing note

--- src/features/Notes/components/Form/index.tsx
+++ src/features/Notes/components/Form/index.tsx
@@ -1,8 +1,20 @@
-import { createStyles, Badge, TextInput, Textarea, Stack, Button, Flex, Paper } from '@mantine/core'
+import {
+  createStyles,
+  Badge,
+  TextInput,
+  Textarea,
+  Stack,
+  Button,
+  Flex,
+  Paper,
+  Modal
+} from '@mantine/core'
 
-import type { NoteProps } from '@/features/Notes/Notes.types'
 import { useNotes } from '@/features/Notes/useNotes'
 import { useRouter } from 'next/router'
+import { useDisclosure } from '@mantine/hooks'
+import { stringify } from 'querystring'
+import { useForm } from '@mantine/form'
 
 const useStyles = createStyles((theme) => ({
   body: {
@@ -35,18 +47,55 @@ const useStyles = createStyles((theme) => ({
 }))
 
 export default function NoteForm() {
-  const { notes, selected, unseleactNote } = useNotes()
+  const [opened, { open, close }] = useDisclosure(false)
+  const { notes, selected, unseleactNote, updateNote, removeNote } = useNotes()
   const router = useRouter()
   const { query } = router
   const { classes } = useStyles()
+
   const note = Object.keys(selected).length !== 0 ? selected : notes.find((n) => n.id == query.id)
   const { id, text, title } = note!
+
+  const form = useForm({
+    initialValues: {
+      title,
+      text
+    }
+  })
+
   function handleUnslectNote() {
     unseleactNote()
     router.push('/notes')
   }
+
+  function handleSaveNote(values: any) {
+    updateNote(id as string, { id, text: values.text, title: values.title } as any)
+    router.push('/notes')
+  }
+
+  function handleRemoveNote() {
+    removeNote(id as string)
+    router.push('/notes')
+  }
+
   return (
-    <Paper bg="gray.4" shadow="xs" p="md">
+    <Paper
+      bg="gray.4"
+      shadow="xs"
+      p="md"
+      component="form"
+      onSubmit={form.onSubmit((values) => handleSaveNote(values))}
+    >
+      <Modal opened={opened} onClose={close} title="Delete Note?" withCloseButton={false} centered>
+        <Flex align="center" justify="center">
+          <Button size="xs" color="red" mx="md" w="100%" onClick={handleRemoveNote}>
+            Yes
+          </Button>
+          <Button size="xs" color="blue" mx="md" w="100%" onClick={close}>
+            Cancel
+          </Button>
+        </Flex>
+      </Modal>
       <Stack justify="space-between" align="scretch">
         <TextInput
           className={classes.textInput}
@@ -55,6 +104,7 @@ export default function NoteForm() {
           label="Title:"
           size="sm"
           variant="unstyled"
+          {...form.getInputProps('title')}
         />
 
         <Textarea
@@ -64,20 +114,21 @@ export default function NoteForm() {
           size="sm"
           variant="unstyled"
           minRows={4}
+          {...form.getInputProps('text')}
         />
 
         <Stack align="flex-start">
           <Flex align="center" justify="space-between" w="100%">
             <Badge>{selected.id == id ? 'editing' : 'in progress'}</Badge>
             <Button.Group>
-              <Button size="xs" color="green">
+              <Button type="submit" size="xs" color="green">
                 Save
               </Button>
               <Button size="xs" onClick={handleUnslectNote}>
                 Cancel
               </Button>
             </Button.Group>
-            <Button size="xs" color="red">
+            <Button size="xs" color="red" onClick={open}>
               Delete
             </Button>
           </Flex>
--- src/features/Notes/useNotes.ts
+++ src/features/Notes/useNotes.ts
@@ -13,10 +13,10 @@ export const useNotes = create<UseNotesStore>((set) => ({
   unseleactNote: () => set({ selected: {} }),
   addNote: (note: NoteProps) => set((state) => ({ notes: [...state.notes, note] })),
   removeNote: (noteId: NoteProps['id']) =>
-    set((state) => ({ notes: [...state.notes.filter((note) => note.id === noteId)] })),
+    set((state) => ({ notes: [...state.notes.filter((note) => note.id !== noteId)] })),
   updateNote: (noteId: NoteProps['id'], note: NoteProps) =>
     set((state) => {
-      const newNotes = state.notes.map((nt) => (nt.id == noteId ? { ...note } : nt))
+      const newNotes = state.notes.map((nt) => (nt.id == noteId ? { ...nt, ...note } : nt))
       return { notes: [...newNotes] }
     }),
   moveToAnotherFolder: (noteId: NoteProps['id'], folderName: NoteFolder) =>
